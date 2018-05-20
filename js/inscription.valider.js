7b6aa4a08e32894a517424928678b9a39c655631
Kvinhtr <37657035+Kvinhtr@users.noreply.github.com>
2018-05-20 18:08:18 +0200

    Delete inscription.valider.js

--- js/inscription.valider.js
+++
@@ -1,9 +0,0 @@
-var frmvalidator  = new Validator("inscription");
-frmvalidator.EnableOnPageErrorDisplay();
-frmvalidator.EnableMsgsTogether();
-frmvalidator.addValidation("nom","req","Veuillez renseigner votre nom");
-frmvalidator.addValidation("prenom","req","Veuillez renseigner votre prénom");
-frmvalidator.addValidation("email","req","Veuillez renseigner votre adresse email");
-frmvalidator.addValidation("email","email","Veuillez renseigner une adresse email valide");
-frmvalidator.addValidation("mdp","req","Veuillez renseigner un mot de passe");
-
